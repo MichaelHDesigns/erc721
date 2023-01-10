8b79b7fdb1c1aa80f2ff1d153d725274e1251401
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-01-09 22:11:30 -0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -46,7 +46,7 @@ module.exports = {
       gasPrice: 2000000000 // 2 gwei
     },
     local: {
-      host: '134.209.116.247',
+      host: '127.0.0.1',
       port: 8545,
       network_id: '2330',
       skipDryRun: true,
